be0938526406abdef0befde8cd6d8afd02e48f73
harikapadia999 <harikapadia999@gmail.com>
2023-08-03 23:25:22 +0530

    Design part done in home search section.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import HomeHeader from './components/HomeHeader'
+import Image from 'next/image'
+import HomeSearch from './components/HomeSearch'
 
 export default function Home() {
   return (
@@ -9,7 +11,14 @@ export default function Home() {
         <meta name="description" content="Google clone created by Next js 13" />
         <link rel="icon" href="/favicon.svg" />
       <>
+        {/* header */}
         <HomeHeader/>
+
+        {/* body */}
+        <div className='flex flex-col items-center mt-24'>
+          <Image width="300" height="100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"/>
+          <HomeSearch/>
+        </div>
       </>
     </div>
   )
