4881a26cb182a599c8eadcd9e2026d9727aee056
harikapadia999 <harikapadia999@gmail.com>
2023-08-03 15:13:58 +0530

    install next js and create template

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    <title>Google Clone Next js 13</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Google clone created by Next js 13" />
+        <link rel="icon" href="/favicon.svg" />
+      <div>Home</div>
+    </div>
+  )
+}
