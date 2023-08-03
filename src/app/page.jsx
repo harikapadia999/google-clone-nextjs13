3e21cf0262747ec61b5e81894e2b4e3105c22359
harikapadia999 <harikapadia999@gmail.com>
2023-08-04 00:40:34 +0530

    Add body section.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import HomeHeader from './components/HomeHeader'
-import Image from 'next/image'
-import HomeSearch from './components/HomeSearch'
+import React from "react";
+import HomeHeader from "./components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "./components/HomeSearch";
 
 export default function Home() {
   return (
     <div>
-    <title>Google Clone Next js 13</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="Google clone created by Next js 13" />
-        <link rel="icon" href="/favicon.svg" />
+      <title>Google Clone Next js 13</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created by Next js 13" />
+      <link rel="icon" href="/favicon.svg" />
       <>
         {/* header */}
-        <HomeHeader/>
+        <HomeHeader />
 
         {/* body */}
-        <div className='flex flex-col items-center mt-24'>
-          <Image width="300" height="100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"/>
-          <HomeSearch/>
+        <div className="flex flex-col items-center mt-24">
+          <Image
+            width="300"
+            height="100"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+            alt="google-logo"
+          />
+          <HomeSearch />
         </div>
       </>
     </div>
-  )
+  );
 }
